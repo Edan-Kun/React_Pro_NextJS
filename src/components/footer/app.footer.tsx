40b3524f2cb86e37d93b8bbd89a473231db2a488
Minh Duc <minhducnguyen.dev@gmail.com>
2024-10-13 09:30:56 +0700

    Fetch Music

--- src/components/footer/app.footer.tsx
+++ src/components/footer/app.footer.tsx
@@ -21,7 +21,7 @@ const AppFooter = () => {
                 sx={{ display: "flex", gap: 10 }}
             >
                 <AudioPlayer
-                    src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/Once_Upon_A_Time.mp3`}
                     volume={0.5}
                     style={{
                         boxShadow: "unset",
