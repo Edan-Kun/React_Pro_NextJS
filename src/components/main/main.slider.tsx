e262bdf54a038f1334d67725eec1ef252022f39b
Minh Duc <minhducnguyen.dev@gmail.com>
2024-10-13 08:46:08 +0700

    SetUp Environment

--- src/components/main/main.slider.tsx
+++ src/components/main/main.slider.tsx
@@ -7,7 +7,6 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import Divider from '@mui/material/Divider';
 
-
 const MainSlider = () => {
     const NextArrow = (props: any) => {
         return (
