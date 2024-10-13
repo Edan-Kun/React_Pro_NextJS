f024a40c6c7330608654f74c19994922d948e5da
Minh Duc <minhducnguyen.dev@gmail.com>
2024-10-13 10:44:22 +0700

    Fetch Wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
+import { sendRequest } from "@/utils/api";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const res = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: "http://localhost:8000/api/v1/tracks/top",
+    method: "POST",
+    body: { category: "Remix", limit: 1 },
+  })
+
   return (
     <Container maxWidth="xl">
       <MainSlider />
