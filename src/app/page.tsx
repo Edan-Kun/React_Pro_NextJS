d3fc42527368c529b0f53730c12354c42b6a7cc1
Minh Duc <minhducnguyen.dev@gmail.com>
2024-10-13 08:28:42 +0700

    Design Main Content và Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import MainSlider from "@/components/main/main.slider";
+import { Container } from "@mui/material";
 
 const HomePage = () => {
   return (
-    <>
-
-    </>
-  );
+    <Container maxWidth="xl">
+      <MainSlider />
+      <MainSlider />
+      <MainSlider />
+    </Container>
+  )
 }
 export default HomePage;
