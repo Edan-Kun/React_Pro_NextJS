aaa2057185c00c82a849c073f516ddcd53d11ace
Minh Duc <minhducnguyen.dev@gmail.com>
2024-10-15 03:39:39 +0700

    Config Session Server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
 import { sendRequest } from "@/utils/api";
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const HomePage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log("Check session: ", session);
+
   const remix = await sendRequest<IBackendRes<ITrackTop[]>>({
     url: "http://localhost:8000/api/v1/tracks/top",
     method: "POST",
